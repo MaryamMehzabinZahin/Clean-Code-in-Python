fecdd738c22ee36be1e916bdaa851115046baa44
MaryamMehzabinZahin <50514699+MaryamMehzabinZahin@users.noreply.github.com>
2022-02-11 17:05:37 +0600

    rewrite functions in snake-case and lower-case

--- Assignments/refactored_code.py
+++ Assignments/refactored_code.py
@@ -2,19 +2,14 @@ from modules import judges
 from modules.apiHandler import ApiCaller
 
 '''
-Refresh offline data
-'''
-
-'''
 Vjudge Login
 '''
+
 vj_username = input("Please enter your vjudge username: ")
 vj_password = input("Please enter your vjudge password: ")
 
-vjudge_user = judges.Vjudge(vj_username, vj_password)
-vjudge_user.downloadSubmissions()
-
-
+vjudge_user = judges.vjudge(vj_username, vj_password)
+vjudge_user.download_submissions()
 
 '''
 UVa Login
@@ -22,8 +17,8 @@ UVa Login
 uva_username = input("Please enter your UVa username: ")
 uva_password = input("Please enter your UVa password: ")
 
-uva_user = judges.UVA(uva_username, uva_password)
-uva_user.submitAll(submit_solved_ones = False, limit_submission_count = 10)
+uva_user = judges.uva(uva_username, uva_password)
+uva_user.submit_all(submit_solved_ones = False, limit_submission_count = 10)
 
 
 
@@ -33,15 +28,15 @@ CodeForces Login
 cf_username = input("Please enter your CodeForces username: ")
 cf_password = input("Please enter your CodeForces password: ")
 
-cf_user = judges.CF(cfusername, cfpassword)
-cf_user.submitAll(submit_solved_ones = False, limit_submission_count = 10)
+cf_user = judges.cf(cfusername, cfpassword)
+cf_user.submitall(submit_solved_ones = False, limit_submission_count = 10)
 
 
 '''
 LightOJ Login
 '''
-loj_user = judges.LOJ()
-loj_user.submitAll(submit_solved_ones = False, limit_submission_count = 10)
+loj_user = judges.loj()
+loj_user.submit_all(submit_solved_ones = False, limit_submission_count = 10)
 
 
 '''
@@ -50,5 +45,5 @@ SPOJ Login
 spoj_username = input("Please enter your SPOJ username: ")
 spoj_password = input("Please enter your SPOJ password: ")
 
-spoj_user = judges.SPOJ(spojusername, spojpassword)
+spoj_user = judges.spoj(spojusername, spojpassword)
 spoj_user.submitAll(submit_solved_ones = False, limit_submission_count = 20)
